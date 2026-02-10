3b05038c92708ebcce55de08c435c7d9c8f8965d
Neeraali-digital <neeraalidigitalweb@gmail.com>
2026-02-10 13:04:45 +0530

    topbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
 import { FooterComponent } from './components/footer/footer';
+import { TopBarComponent } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, TopBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
